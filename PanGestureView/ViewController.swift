e512c1abdde428cd06a912c9131d16d05408bbff
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2016-01-30 18:47:22 +0530

    Initial Commit

---
+++ PanGestureView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PanGestureView
+//
+//  Created by Arvindh Sukumar on 30/01/16.
+//  Copyright © 2016 Arvindh Sukumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
